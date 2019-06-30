99d42a9ac2824d11872a3f5929f4bff14fb04040
zkyzq <37827813+zkyzq@users.noreply.github.com>
2019-06-30 15:49:38 +0800

    增加反向传播更新参数部分

--- learn_FM/FM.py
+++ learn_FM/FM.py
@@ -15,3 +15,17 @@ for it in range(self.iter): # 迭代次数
         # 计算预测
         pred = w_0 + mat(x) * w + interaction
         print("预测的输出pred:", pred)
+        # 计算sigmoid(y*pred_y)-1准确的说不是loss，原作者这边理解的有问题，只是作为更新w的中间参数，这边算出来的是越大越好，而下面却用了梯度下降而不是梯度上升的算法在
+        loss = self.sigmoid(classLabels[x] * p[0, 0]) - 1
+        if loss >= -1: # 暂时不明白这段作用 
+            loss_res = '正方向 '
+        else:
+            loss_res = '反方向'
+        # 更新参数
+        w_0 = w_0 - self.alpha * loss * classLabels[x] # 常数项系数更新
+        for i in range(n):
+            if dataMatrix[x, i] != 0:
+                w[i, 0] = w[i, 0] - self.alpha * loss * classLabels[x] * dataMatrix[x, i] # 一次项系数更新
+                for j in range(k):
+                    v[i, j] = v[i, j] - self.alpha * loss * classLabels[x] * (
+                            dataMatrix[x, i] * inter_1[0, j] - v[i, j] * dataMatrix[x, i] * dataMatrix[x, i]) # 交叉项系数更新
