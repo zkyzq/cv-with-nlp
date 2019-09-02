6e23c563ace854026894cf9ddfd875e2df910f00
buaazhangqi <2445657635@qq.com>
2019-09-02 09:39:03 +0800

    初识IO流

---
+++ learn-java/MyIO.java
@@ -0,0 +1,12 @@
+IO流是一种流式的数据输入/输出模型：
+
+二进制数据以byte为最小单位在InputStream/OutputStream中单向流动；
+
+字符数据以char为最小单位在Reader/Writer中单向流动。
+
+Java标准库的java.io包提供了同步IO功能：
+Java标准库的java.nio包提供了异步io功能
+
+字节流接口：InputStream/OutputStream；
+
+字符流接口：Reader/Writer。
